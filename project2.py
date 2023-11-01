2ae80e3406130ff3a56fba3573e2951273efa14c
JuliaMarleen <aan_marleen@hotmail.com>
2023-11-01 14:21:15 +0100

    “functions”

--- project2.py
+++ project2.py
@@ -8,9 +8,15 @@ def calculation(a, b):
     return c
 
 
+def outcome(number):
+    sentence = "The outcome is " + str(number)
+    return sentence
+
+
 def main():
     print("python main function")
     print(calculation(3, 4))
+    print(outcome(calculation(6, 7)))
 
 
 if __name__ == '__main__':
